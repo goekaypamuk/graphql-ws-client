992dc4034426d2032cde8723d5df322da85c6298
Goekay Pamuk <pag@informatec.com>
2020-07-25 21:29:38 +0200

    re-init on error

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ module.exports =  (params) => {
     });
 
     webSocket.on('close', () => {
+        if (params.selfDestroy) {
+            process.exit(1);
+        }
         deferred = new Deferred();
     });
 
